50f60963e97b8fb1c3331ca6d60e44925e57d307
Kouhei Sutou <kou@clear-code.com>
2017-09-14 00:45:55 +0900

    Add more contents

--- benchmark/sum.rb
+++ benchmark/sum.rb
@@ -3,8 +3,12 @@ require "benchmark"
 numbers = (1..100000).to_a
 
 Benchmark.bmbm do |job|
-  job.report("inject") do
+  job.report("inject (block)") do
     numbers.inject(&:+)
+  end
+
+  job.report("inject (symbol)") do
+    numbers.inject(:+)
   end
 
   job.report("sum") do
